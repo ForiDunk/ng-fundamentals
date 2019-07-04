589c24165efa48d6a4819e228c5b95a5ec5c25fd
ForiDunk <szabolcs.forreiter@gmail.com>
2019-07-04 16:11:24 +0300

    start

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+// tslint:disable-next-line: component-selector
+  selector: 'events-app',
+  template: `
+    <h1>Heloooooo</h1>
+  `
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
