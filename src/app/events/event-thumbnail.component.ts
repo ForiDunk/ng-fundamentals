483f3ec6ffd0597d021ccb904e10647a010a1581
ForiDunk <szabolcs.forreiter@gmail.com>
2019-07-05 14:34:12 +0300

    ngStylezzzz

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,16 +4,24 @@ import { Component, Input } from '@angular/core';
 // tslint:disable-next-line: component-selector
   selector: 'event-thumbnail',
   template: `
-  <div class="well hoverwell thumbnail">
-  <h2>{{ event.name }}</h2>
-  <div>Date: {{ event.date }}</div>
-  <div>Time: {{ event.time }}</div>
-  <div>Price: {{ event.price | currency }}</div>
-  <div>
-    <span>Location: {{ event.location.address }}</span>
-    <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
+  <div *ngIf="event" class="well hoverwell thumbnail">
+    <h2>{{ event.name }}</h2>
+    <div>Date: {{ event.date }}</div>
+    <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event.time">
+      Time: {{ event.time }}
+      <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+      <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+      <span *ngSwitchDefault>(Normal Start)</span>
+    </div>
+    <div>Price: {{ event.price | currency }}</div>
+    <div *ngIf="event.location">
+      <span>Location: {{ event.location.address }}</span>
+      <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
+    </div>
+    <div *ngIf="event?.onlineUrl">
+      Online URL: {{ event?.onlineUrl }}
+    </div>
   </div>
-</div>
   `,
   styles: [`
     .thumbnail { min-height: 210px; }
@@ -23,4 +31,11 @@ import { Component, Input } from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === '8:00 am') {
+      return {color: '#003300', fontWeight: 'bold'};
+    }
+    return {};
+  }
 }
