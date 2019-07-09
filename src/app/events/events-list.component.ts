ea1dc3aec390b5bbef4c571586302b8944f6ffac
ForiDunk <szabolcs.forreiter@gmail.com>
2019-07-09 10:51:39 +0300

    pre-load

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   template: `
@@ -16,13 +16,13 @@ import { ToastrService } from '../common/toastr.service';
   `
 })
 export class EventsListComponent implements OnInit {
-  events: any[];
+  events: any;
 
-  constructor(private eventService: EventService,
+  constructor(private route: ActivatedRoute,
               private toastr: ToastrService) {}
 
   ngOnInit() {
-    this.events = this.eventService.getEvents();
+    this.events = this.route.snapshot.data.events;
   }
 
   handleThumbnailClick(eventName) {
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
 
 const EVENTS = [
   {
@@ -312,8 +313,13 @@ const EVENTS = [
 })
 export class EventService {
 
-  getEvents() {
-    return EVENTS;
+  getEvents(): Observable<any> {
+    const subject = new Subject();
+    setTimeout(() => {
+      subject.next(EVENTS);
+      subject.complete();
+    }, 100);
+    return subject;
   }
 
   getEvent(id: number) {
