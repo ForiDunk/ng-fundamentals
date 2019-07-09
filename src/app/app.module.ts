38ece6bc0851923c4f3b7d65eb02690443f2e8c8
ForiDunk <szabolcs.forreiter@gmail.com>
2019-07-09 15:28:57 +0300

    org exports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent
+} from './events/index';
 
 
 @NgModule({
