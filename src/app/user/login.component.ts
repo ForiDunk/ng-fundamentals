7e6faf4dcd3db8f0143851a549761be56921be6d
ForiDunk <szabolcs.forreiter@gmail.com>
2019-07-10 09:13:55 +0300

    login template

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  username: string;
+  userName: string;
   password: string;
 
-  login(data): void {
-    console.log(data);
+  constructor(private authService: AuthService,
+              private router: Router) {}
+
+  login(formValues): void {
+    this.authService.loginUser(formValues.userName, formValues.password);
+    this.router.navigate(['events']);
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
   }
 }
